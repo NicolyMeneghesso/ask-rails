2550f939a8d58f1b9f04f1158530bf990a70ccd1
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2025-02-17 21:06:04 -0300

    Ajustes notice

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,6 +5,4 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "admins_backoffice/admin.js"
-pin "users_backoffice/custom.js"
-pin "panel"
+
