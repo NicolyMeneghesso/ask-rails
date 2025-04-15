04e53104a239078b8e9f836bef92447b67ee6f83
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2025-04-15 19:00:53 -0300

    ajuste api

--- app/javascript/panel/answers.js
+++ app/javascript/panel/answers.js
@@ -1,29 +1,37 @@
 //JS do answer, onde tem um api para buscar as questions, e a função de clicar e abrir o card com as peruntas
 
 // Declaramos a função no objeto global 'window' para que possa ser acessada pelo HTML inline (ex: onclick="LoadCardQuestion(...)")
-window.LoadCardQuestion = function(id) { 
+window.loadCardQuestion = function(id) { 
    let cardQuestions = document.getElementById('cardQuestion');
    cardQuestions.style.display = "block";
-   loadData(id);
+   loadDataQuestion(id);
 };
 
-async function loadData(subject_id) {
+async function loadDataQuestion(subject_id) {
    try {
       const questions = await fetch(`/api/questions?subject_id=${subject_id}`) // ele chama a requisição
       if (!questions.ok) throw new Error('Erro na API');
 
-      const data = await questions.json() // ele pega a requisição e transforma em json
+      const dataQuestion = await questions.json() // ele pega a requisição e transforma em json
 
-      let container = document.getElementById('questions')
-      container.innerHTML = ''  // limpa conteúdo anterior
+      let tableBody = document.getElementById('tableQuestions');
+      tableBody.innerHTML = ''  // limpa conteúdo anterior
 
-      data.forEach(questionAndAnswer => {
-         let p = document.createElement('p')
-         p.textContent = questionAndAnswer.description
-         container.appendChild(p)
+      dataQuestion.forEach(question => { //pega a array e percorre ela, e a variavel question representa uma pergunta
+         const tr = document.createElement('tr');
+         const td = document.createElement('td');
+         td.textContent = question.description;
+         tr.appendChild(td);
+         tableBody.appendChild(tr);
       });
-
+      
    } catch (erro) {
       console.error('Erro ao buscar os dados:', erro);
    }
 }
+
+window.loadCardAnswer = function(id_answer) { 
+   let cardAnswer = document.getElementById('cardQuestion');
+   cardAnswer.style.display = "block";
+   loadDataAnswer(id_answer);
+};
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :questions do
       get "/", action: :index
+      get "/answers", action: :answers
     end
   end
 
