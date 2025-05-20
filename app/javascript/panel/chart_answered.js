4b92884d131fbc7ccb86bdff266ee32e3fc215f2
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2025-05-20 19:55:20 -0300

    ajustes

--- app/javascript/panel/chart_answered.js
+++ app/javascript/panel/chart_answered.js
@@ -1,10 +1,13 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const canvas = document.getElementById("questionsChart")
-    const graphics = canvas.getContext("2d")
+   const canvas = document.getElementById("questionsChart")
+   const graphics = canvas.getContext("2d")
 
-    // Chart e um objeto da biblioteca Chart.js
-    const chart = new Chart(graphics, {
-        type: 'doughnut', //Define o tipo de gráfico 
-    })
-    
+   // Chart e um objeto da biblioteca Chart.js
+   const chart = new Chart(graphics, {
+      type: 'doughnut', //Define o tipo de gráfico 
+      data: {
+         labels: ['Certas', 'Erradas'],
+      }
+   })
+   
 })
