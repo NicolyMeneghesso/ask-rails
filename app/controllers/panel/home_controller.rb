4d4f52d0a402d6573f378edaed5b376fc463d7c8
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2025-05-19 21:20:24 -0300

    ajuste page panel

--- app/controllers/panel/home_controller.rb
+++ app/controllers/panel/home_controller.rb
@@ -1,5 +1,6 @@
 class Panel::HomeController < PanelBaseController
   def index
     @user_statistic = UserStatistic.find_or_create_by(user: current_user)
+    @total_questions_count = Question.all.size
   end
 end
