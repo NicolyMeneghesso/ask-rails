0c7d00cd02ea3db772be647c683b1dacda765b0d
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2025-05-20 21:29:34 -0300

    ajuste page panel user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  skip_forgery_protection
 
   protected
   # Depois que o usuário fizer login com sucesso, redirecione ele para panel_home_index_path
