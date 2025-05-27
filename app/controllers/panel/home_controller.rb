4edcd01948580846ae40de4fed15f1c0cb333619
Nicoly Meneghesso <nihmeneghesso@gmail.com>
2025-05-27 20:15:01 -0300

    ajuste page admin

--- app/controllers/panel/home_controller.rb
+++ app/controllers/panel/home_controller.rb
@@ -9,11 +9,20 @@ class Panel::HomeController < PanelBaseController
   private
 
   def set_admin_data
+    # total de usuarios para o card
     @user_counts = User.group(:user_type).count
+    # total de assuntos para o card
     @total_subjects = Subject.count
 
+    # mostra o total de perguntas respondidas
     @total_users_statistic_right = UserStatistic.sum(:right_questions)
     @total_users_statistic_wrong = UserStatistic.sum(:wrong_questions)
-    @total_users_statistic = @total_users_statistic_right + @total_users_statistic_wrong
+    @total_answers = @total_users_statistic_right + @total_users_statistic_wrong
+
+    # 1. Pega todos os UserStatistic de usuários com user_type: 0 (usuários comuns)
+    @users_can_answer = UserStatistic.where(user_id: User.where(user_type: 0).select(:id))
+
+    # 2. Filtra os que realmente responderam alguma pergunta (acertando ou errando)
+    @users_who_answered = @users_can_answer.where("right_questions > 0 OR wrong_questions > 0").count
   end
 end
